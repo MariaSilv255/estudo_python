ca130b09eefd3d68a4fb9ed550d0bc7e1c76d615
Maria Aparecida <maria.info108@gmail.com>
2025-03-26 22:25:58 -0300

    Basico de python

---
+++ mundo_01_python/aula_4.py
@@ -0,0 +1,18 @@
+nome = input('Qual é a sua nome?')
+idade = input('qua é a sua idade?')
+peso = input('qual é o seu peso?')
+
+print(nome,idade,peso)
+
+print('seja bem vindo(a)',nome)
+
+dia = input('dia do nascimento')
+mes = input('mes do nascimento')
+ano = input('ano do nascimento')
+
+print('Voce nasceu em',dia,'/',mes,'/',ano)
+
+n1 = float(input('digite um numero'))
+n2 = float(input('digite um numero'))
+n3 = float(input('digite um numero'))
+print(n1+n2+n3)
---
+++ mundo_01_python/aula_6.py
@@ -0,0 +1,21 @@
+#tipos primitivos e saida de dados
+#int: 7,-4,0,4844
+# float: 4.5,0.05,-12.5,7.9
+# bool: True, False
+# str:'Olá'
+
+
+n1 = input('Digite um numero')
+n2 = input('Digite um numero')
+n3 = input('digite um numero')
+
+print('Soma vale',n1 + n2 + n3)
+
+#tipo int
+
+n1 = int(input('Digite um numero'))
+n2 = int(input('Digite um numero'))
+
+#print(type(n1))#verifica o tipo da variavel
+print('Soma entre {} e {} vale {}'.format(n1, n2, n1+n2)) #recomendado
+
